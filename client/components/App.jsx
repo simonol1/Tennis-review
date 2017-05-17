b4efd25c161f494872a717fe9168c265a7269799
simonol1 <simondanolearyis@gmail.com>
2017-05-18 11:57:29 +1200

    added react component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import {HashRouter as Router} from 'react-router-dom'
+
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <h1>Review My Tennis Match</h1>
   )
 }
 
 export default App
-
